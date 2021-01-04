295226dd6d17096fa89fa13c013d92357701c228
Alexej Bodnarchuk <alexej1900@mail.ru>
2021-01-04 18:32:30 +0300

    fix: corrected avg

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-
-exports.min = function min (array) {
-  let min = 0;
-  if (arguments.length == 0) min = 0;
-  else for (i=0; i<array.length; i++){
-        if (array[i] < min) min = array[i];
-    }
-    return (arguments.length == 0)? 0:min;
+exports.min = function min(array) {
+    let min = 0;
+    if (arguments.length == 0) min = 0;
+    else
+        for (i = 0; i < array.length; i++) {
+            if (array[i] < min) min = array[i];
+        }
+    return (arguments.length == 0) ? 0 : min;
 }
 
-exports.max = function max (array) {
- let max = 0;
+exports.max = function max(array) {
+    let max = 0;
     if (arguments.length == 0) max = 0;
-    else for (i=0; i<array.length; i++){
-        if (array[i] > max) max = array[i];
-    }
-    return (arguments.length == 0)? 0:max;
+    else
+        for (i = 0; i < array.length; i++) {
+            if (array[i] > max) max = array[i];
+        }
+    return (arguments.length == 0) ? 0 : max;
 }
 
-exports.avg = function avg (array) {
-  let avg = 0;
-  if (arguments.length == 0) avg = 0;
-  else for (i=0; i<array.length; i++){
-        avg += array[i];
-    }
-return (arguments.length == 0)? 0:avg/array.length;
+exports.avg = function avg(array) {
+    let avg = 0;
+    if (arguments.length == 0 || array.length == 0) avg = 0;
+    else
+        for (i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+    return (arguments.length == 0 || array.length == 0) ? avg : avg / array.length;
 }
