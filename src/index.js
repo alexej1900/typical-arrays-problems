ca58fa5ab8561865afcb35867152e7a41e31b967
Alexej Bodnarchuk <71667926+alexej1900@users.noreply.github.com>
2021-01-04 15:42:50 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if (arguments.length == 0) min = 0;
+  else for (i=0; i<array.length; i++){
+        if (array[i] < min) min = array[i];
+    }
+    return (arguments.length == 0)? 0:min;
 }
 
 exports.max = function max (array) {
-  return 0;
+ let max = 0;
+    if (arguments.length == 0) max = 0;
+    else for (i=0; i<array.length; i++){
+        if (array[i] > max) max = array[i];
+    }
+    return (arguments.length == 0)? 0:max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  if (arguments.length == 0) avg = 0;
+  else for (i=0; i<array.length; i++){
+        avg += array[i];
+    }
+return (arguments.length == 0)? 0:avg/array.length;
 }
